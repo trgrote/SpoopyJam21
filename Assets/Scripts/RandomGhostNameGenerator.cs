b23abd8e40f01c901b94b89851023d39c5e2185d
Taylor Grote <grotese@gmail.com>
2021-10-10 17:59:49 -0500

    Add random name generator test

--- Assets/Scripts/RandomGhostNameGenerator.cs
+++ Assets/Scripts/RandomGhostNameGenerator.cs
@@ -5,31 +5,37 @@ using UnityEngine;
 
 public class RandomGhostNameGenerator : MonoBehaviour
 {
-    public float _prefixChance = 0.2f;
-    public float _firstNameChance = 1f;
-    public float _lastNameChance = 0.8f;
-    public float _sufffixChance = 0.2f;
+    private float _prefixChance = 0.1f;
+    private float _firstNameChance = 1f;
+    private float _lastNameChance = 0.8f;
+    private float _sufffixChance = 0.1f;
 
     public Repository.GhostNameRepository _ghostNames;
 
     public string GenerateRandomName()
     {
         var prefix = Random.value <= _prefixChance ? 
-            _ghostNames.Names.Where(n => n.Prefix).OrderBy(n => Random.value).Select(n => name).FirstOrDefault() :
+            _ghostNames.Names.Where(n => n.Prefix).OrderBy(n => Random.value).Select(n => n.Name).FirstOrDefault() :
             "";
-
+       
         var first = Random.value <= _firstNameChance ? 
-            _ghostNames.Names.Where(n => n.FirstName).OrderBy(n => Random.value).Select(n => name).FirstOrDefault() :
+            _ghostNames.Names.Where(n => n.FirstName).OrderBy(n => Random.value).Select(n => n.Name).FirstOrDefault() :
             "";
             
         var last = Random.value <= _lastNameChance ? 
-            _ghostNames.Names.Where(n => n.LastName).OrderBy(n => Random.value).Select(n => name).FirstOrDefault() :
+            _ghostNames.Names.Where(n => n.LastName).OrderBy(n => Random.value).Select(n => n.Name).FirstOrDefault() :
             "";
 
         var suffix = Random.value <= _sufffixChance ? 
-            _ghostNames.Names.Where(n => n.Suffix).OrderBy(n => Random.value).Select(n => name).FirstOrDefault() :
+            _ghostNames.Names.Where(n => n.Suffix).OrderBy(n => Random.value).Select(n => n.Name).FirstOrDefault() :
             "";
 
         return string.Join(" ", new string[]{prefix, first, last, suffix}.Where(n => n.Length > 0));
+    }
+
+    [NaughtyAttributes.Button]
+    public void TestRandomName()
+    {
+        Debug.Log(GenerateRandomName());
     }
 }
